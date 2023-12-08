b491746a6d79c111aeb3f9a4b9d286d3aa9149f8
soapAndOoks <151615938+soapAndOoks@users.noreply.github.com>
2023-12-08 08:13:05 +0000

    Update pictureReplacer.js

--- pictureReplacer.js
+++ pictureReplacer.js
@@ -3,14 +3,14 @@
 var counter = 0
 
 function addRandomBackgroundPicture(){
-  document.body.style.backgroundImage = "url('https://picsum.photos/200/300?random=" + i + "')"
+  document.body.style.backgroundImage = "url('https://picsum.photos/200/300?random=" + counter + "')"
   //to generate unique images each time
   counter++
 }
 
 function replaceImages(){
   for (var image of document.getElementsByTagName("img")){
-    image.src='https://picsum.photos/200/300?random=' + i
+    image.src='https://picsum.photos/200/300?random=' + counter
     counter++
   }
 }
