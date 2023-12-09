c415fe1abf4855a0d6e567385d3670bcc224bc22
soapAndOoks <151615938+soapAndOoks@users.noreply.github.com>
2023-12-09 20:35:53 +0000

    Create TextReplacer.js

---
+++ TextReplacer.js
@@ -0,0 +1,52 @@
+'use strict'
+
+function generateRandomMessage(){
+  let messages = ["", "Have you tried searching up what a virus is?", "Error", "0xDF6 crash"];
+  let message = messages[Math.floor(Math.random()*messages.length)];
+  return message;
+}
+  
+function getAllTags(){
+  let tags = []
+  for (let tag of document.getElementsByTagName("p")){tags.push(tag);}
+  for (let tag of document.getElementsByTagName("h1")){tags.push(tag);}
+  for (let tag of document.getElementsByTagName("h2")){tags.push(tag);}
+  for (let tag of document.getElementsByTagName("h3")){tags.push(tag);}
+  for (let tag of document.getElementsByTagName("h4")){tags.push(tag);}
+  for (let tag of document.getElementsByTagName("h5")){tags.push(tag);}
+  for (let tag of document.getElementsByTagName("h6")){tags.push(tag);}
+  for (let tag of document.getElementsByTagName("a")){tags.push(tag);}
+  return tags;
+}
+
+function replaceOneTagText(tag, delay){
+  setTimeout(() => {
+    tag.innerHTML = generateRandomMessage();
+  }, delay);
+}
+
+  
+function slowlyReplaceAllText(timeTaken){
+  let allTags = getAllTags();
+  const timePerItem = timeTaken/allTags.length;
+  let delay = 0;
+  while (allTags.length){
+    let i = Math.floor(Math.random() * allTags.length);
+    replaceOneTagText(allTags[i], delay);
+    allTags.splice(i, 1);
+    delay += timePerItem;
+  }
+}
+
+function cycleText(){
+  let allTags = getAllTags();
+  let delay = 0;
+  while (allTags.length){
+    let i = Math.floor(Math.random() * allTags.length);
+    replaceOneTagText(allTags[i], delay);
+    allTags.splice(i, 1);
+    delay += 0.05;
+  }
+}
+
+export {slowlyReplaceAllText, cycleText}
