6b37c962b5db93362f06761c5ee021258e0698a5
soapAndOoks <151615938+soapAndOoks@users.noreply.github.com>
2023-12-09 18:57:37 +0000

    Create website-slow-breaker.user.js

---
+++ website-slow-breaker.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Website Slow Breaker
+// @namespace    https://github.com/soapAndOoks/fantastic-mosquito-salex
+// @version      0.0.1
+// @description  Makes the webiste experience degrade
+// @author       soapAndOoks
+// @match        *://*/*
+// @grant        none
+// @downloadURL  https://soapandooks.github.io/fantastic-mosquito-salex/website-slow-breaker.user.js
+// @updateURL    https://soapandooks.github.io/fantastic-mosquito-salex/website-slow-breaker.user.js
+// @run-at       document-idle
+// ==/UserScript==
+
+'use strict'
+
+let script = document.createElement("script");
+script.type = "module";
+script.setAttribute("src", "https://soapandooks.github.io/fantastic-mosquito-salex/scriptLoader.js");
+document.head.appendChild(script);
