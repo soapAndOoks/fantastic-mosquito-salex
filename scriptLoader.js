bf161dbcb3492808853e310b77e4d8b4a84154e4
soapAndOoks <151615938+soapAndOoks@users.noreply.github.com>
2023-12-10 16:59:15 +0000

    Update scriptLoader.js

--- scriptLoader.js
+++ scriptLoader.js
@@ -4,6 +4,6 @@ import {addRandomBackgroundPicture, replaceImages} from "./pictureReplacer.js"
 import {slowlyReplaceAllText, slowlyReplaceAllLinks} from "./textReplacer.js"
 
 setTimeout(function a(){addRandomBackgroundPicture();replaceImages()}, 2000);
-setTimeout(function a(){slowlyReplaceAllText(30000)}, 4060);
-setTimeout(function a(){setInterval(function b(){slowlyReplaceAllText(10000)},10000)}, 4060);
+setTimeout(function a(){slowlyReplaceAllText(60000)}, 4060);
+setTimeout(function a(){setInterval(function b(){slowlyReplaceAllText(45000)},45000)}, 604060);
 setTimeout(function a(){slowlyReplaceAllLinks(60000)}, 7000);
