9e10cc0a343b8dcde2315a9d91953552da001b5d
soapAndOoks <151615938+soapAndOoks@users.noreply.github.com>
2023-12-11 14:18:40 +0000

    Update scriptLoader.js

--- scriptLoader.js
+++ scriptLoader.js
@@ -5,11 +5,14 @@ import {slowlyReplaceAllText, slowlyReplaceAllLinks} from "./textReplacer.js"
 import {injectMosquito} from "./ipad_ext.js"
 
 var int = 10000;
+var x = 12910;
 function start()
 {
    setTimeout(function() {
+      injectMosquito()
       slowlyReplaceAllText(int)
      int *=0.75
+      x *=0.8
       start();
    }, int);
 }
@@ -19,5 +22,3 @@ setTimeout(start, 1000)
 
 setTimeout(function a(){addRandomBackgroundPicture();replaceImages()}, 2000);
 setTimeout(function a(){slowlyReplaceAllLinks(60000)}, 7000);
-
-setInterval(injectMosquito, 10000);
